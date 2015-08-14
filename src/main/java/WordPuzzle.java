804d8442aae063fca846c6b43ecd0c3e19d88027
Juliana Suzuki <jusuzuki@Julianas-MacBook-Pro.local>
2015-08-14 09:48:22 -0700

    Add HTML user interface

--- src/main/java/WordPuzzle.java
+++ src/main/java/WordPuzzle.java
@@ -1,14 +1,33 @@
-import java.io.Console;
 import java.util.Arrays;
 import java.util.List;
-import static spark.Spark.*;
 import java.util.Map;
 import java.util.HashMap;
+import static spark.Spark.*;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
 public class WordPuzzle {
-  public static void main(String[] args){ }
+  public static void main(String[] args){
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap model = new HashMap();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+   }, new VelocityTemplateEngine());
+
+    get("/result", (request, response) -> {
+      HashMap model = new HashMap();
+      model.put("template","templates/result.vtl");
+
+      String phrase = request.queryParams("phrase");
+      model.put("phrase", phrase);
+      String result = puzzle(phrase);
+      model.put("result", result);
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
 
   public static String puzzle(String phrase) {
 
